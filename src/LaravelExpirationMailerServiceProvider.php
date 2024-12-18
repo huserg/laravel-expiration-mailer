225e52d09442bef1db1d653efc280cd704218273
HuserG <gaetan.huser@bm-s.ch>
2024-12-18 13:34:54 +0100

    Fix boot method scheduler

--- src/LaravelExpirationMailerServiceProvider.php
+++ src/LaravelExpirationMailerServiceProvider.php
@@ -4,6 +4,7 @@ namespace HuserG\LaravelExpirationMailer;
 
 use HuserG\LaravelExpirationMailer\Commands\SendExpirationEmails;
 use HuserG\LaravelExpirationMailer\Models\Expiration;
+use HuserG\LaravelExpirationMailer\Scheduler\TaskScheduler;
 use Illuminate\Console\Scheduling\Schedule;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -33,34 +34,14 @@ class LaravelExpirationMailerServiceProvider extends PackageServiceProvider
             });
     }
 
+
     public function boot(): void
     {
+        parent::boot();
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'expiration-mailer');
-
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/expiration-mailer'),
-        ], 'expiration-mailer-views');
-
-        // Planifier l'exécution de la commande
         $this->app->booted(function () {
             $schedule = $this->app->make(Schedule::class);
-
-            // Planification : 30 jours avant expiration
-            $schedule->command(SendExpirationEmails::class)
-                ->daily()
-                ->when(fn () => $this->shouldSendEmails(30));
-
-            // Planification : 14 jours avant expiration
-            $schedule->command(SendExpirationEmails::class)
-                ->daily()
-                ->when(fn () => $this->shouldSendEmails(14));
+            TaskScheduler::register($schedule);
         });
-    }
-
-    protected function shouldSendEmails(int $daysBefore): bool
-    {
-        // Vérifier s'il y a des expirations pour X jours avant la date
-        return Expiration::whereDate('expiration_date', now()->addDays($daysBefore)->toDateString())->exists();
     }
 }
---
+++ src/Scheduler/TaskScheduler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HuserG\LaravelExpirationMailer\Scheduler;
+
+use HuserG\LaravelExpirationMailer\Commands\SendExpirationEmails;
+use Illuminate\Console\Scheduling\Schedule;
+use HuserG\LaravelExpirationMailer\Models\Expiration;
+
+class TaskScheduler
+{
+    public static function register(Schedule $schedule): void
+    {
+        // 30 jours avant expiration
+        $schedule->command(SendExpirationEmails::class)
+            ->daily()
+            ->when(fn () => self::shouldSendEmails(30));
+
+        // 14 jours avant expiration
+        $schedule->command(SendExpirationEmails::class)
+            ->daily()
+            ->when(fn () => self::shouldSendEmails(14));
+    }
+
+    protected static function shouldSendEmails(int $daysBefore): bool
+    {
+        return Expiration::whereDate('expiration_date', now()->addDays($daysBefore)->toDateString())->exists();
+    }
+}
